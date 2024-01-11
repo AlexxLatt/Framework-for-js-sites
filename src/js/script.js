fa9910a917c02dfb408faa91311a36ece9ee8843
AlexLatt <aleksandlatypov@gmail.com>
2024-01-11 18:21:08 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+import slider from "./modules/slider";
+import forms from "./modules/forms"
+import stages from "./modules/stages";
+import tabs from "./modules/tabs";
+import scrollUp from "./modules/scrollUp";
+import mask from "./modules/mask";
+window.addEventListener("DOMContentLoaded", () =>{
+
+
+    mask();
+    tabs();
+    slider();
+    forms();
+    stages();
+    scrollUp();
+
+
+});
